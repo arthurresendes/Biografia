b0f2e5bdff98644d9ff5d67a6dc07ace2eb16300
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2024-06-13 16:40:38 -0300

    Atualização

--- envia.php
+++ envia.php
@@ -1,27 +1,29 @@
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-    $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_STRING);
-    
 
-    if ($nome && $email ) {
-        $para = "arthur.resende.gomes02@gmail.com";
-        $assunto = "Fale comigo - Portfolio";
+if(isset($_POST['email']) && ! empty($_POST['nome'])){
 
-        $corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Telefone: " . $telefone;
-        $cabeca = "From: arthur.resende.gomes1@gmail.com\n" . "Reply-to: " . $email . "\n" . "X-Mailer: PHP/" . phpversion();
+$nome = addslashes($_POST['nome'])
+$email = addslashes($_POST['email'])
+$mensagem = addslashes($_POST['msg'])
 
-        if (mail($para, $assunto, $corpo, $cabeca)) {
-            echo("E-mail enviado com sucesso!");
-        } else {
-            echo("Houve um erro ao enviar o e-mail!");
-        }
-    } else {
-        echo("Dados inválidos. Por favor, verifique suas entradas.");
-    }
-} else {
-    echo("Método de requisição não permitido. Use o método POST.");
+$to = "arthur.resende.gomes02@gmail.com";
+$subject = "Contato - gomes1";
+
+
+$body = "Nome: ".$nome. "\n"
+        "Email: ".$email. "\n"
+        "Mensagem: ".$msg;
+$header = "From: arthur.resende.gomes1@gmail.com"."\r\n". "Reply-To:"$email."\r\n" ."X=Mailer:PHP/".phpversion();
+
+if(mail($to,$subject,$body,$header)) {
+    echo("Email enviado com sucesso");
+}else{
+    echo(O email não pode ser enviado);
 }
+
+}
+
 ?>
+
+
+
