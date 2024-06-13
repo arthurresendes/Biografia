861a7d2d4a91910f2b71b8d81f01a8d72428850d
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2024-06-13 16:12:36 -0300

    Atualização

--- envia.php
+++ envia.php
@@ -4,13 +4,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
     $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_STRING);
-    $mensagem = filter_input(INPUT_POST, 'msg', FILTER_SANITIZE_STRING);
+    
 
-    if ($nome && $email && $msg) {
+    if ($nome && $email ) {
         $para = "arthur.resende.gomes02@gmail.com";
         $assunto = "Fale comigo - Portfolio";
 
-        $corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Telefone: " . $telefone . "\n" . "Mensagem: " . $mensagem;
+        $corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Telefone: " . $telefone;
         $cabeca = "From: arthur.resende.gomes1@gmail.com\n" . "Reply-to: " . $email . "\n" . "X-Mailer: PHP/" . phpversion();
 
         if (mail($para, $assunto, $corpo, $cabeca)) {
