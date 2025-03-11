494eba954d1b641dc9cff613c37abb9d9792d3c0
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-03-10 23:47:00 -0300

    Atualização

---
+++ enviar.php
@@ -0,0 +1,22 @@
+<?php
+
+$nome = addslashes($_POST('nome'));
+$email = addslashes($_POST('email'));
+$telefone = addslashes($_POST('telefone'));
+
+$para = "arthur.resende.gomes02@gmail.com";
+$assunto = "Coleta de dados - Portfolio";
+
+$corpo = "Nome: ".$nome."\n"."E-mail :".$email."\n"."Telefone: ".$telefone;
+
+$cabeca = "From: arthur.resende.gomes01@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+if(mail($para,$assunto,$corpo,$cabeca)){
+    echo("E-mail enviado com sucesso!");
+}
+else{
+    
+}
+ 
+
+?>
